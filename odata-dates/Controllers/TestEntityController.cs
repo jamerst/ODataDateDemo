7acc2ce61af7c8397045de7e35348f92f1f2f893
James Tattersall <10601770+jamerst@users.noreply.github.com>
2022-07-20 21:40:09 +0100

    Add query endpoint

--- odata-dates/Controllers/TestEntityController.cs
+++ odata-dates/Controllers/TestEntityController.cs
@@ -1,8 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 
 public class TestEntityController : ODataController
 {
+    private List<TestEntity> _entities = new List<TestEntity>()
+    {
+        new TestEntity { Id = 1, Date = DateTime.Parse("2022-01-01T12:00:00Z")},
+        new TestEntity { Id = 2, Date = DateTime.Parse("2021-02-01T15:00:00Z")},
+        new TestEntity { Id = 3, Date = DateTime.Parse("2023-01-02T12:31:25Z")},
+        new TestEntity { Id = 4, Date = DateTime.Parse("2022-07-01T13:00:00Z")},
+        new TestEntity { Id = 5, Date = DateTime.Parse("2022-08-01T03:00:00Z")},
+    };
+
+    [HttpGet]
+    [EnableQuery]
+    public IActionResult Get()
+    {
+        return Ok(_entities);
+    }
+
     [HttpPost]
     public IActionResult Post([FromBody] TestEntity testEntity)
     {
